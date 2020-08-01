cf32658e6407c0d3886517029ebfd454f1a9e220
Kirito64 <jatinchhangani@gmail.com>
2020-08-01 15:51:28 +0530

    fixed Controller Version

--- api/app.js
+++ api/app.js
@@ -30,7 +30,6 @@ app.use((err, req, res, next) => {
   // set locals, only providing error in development
   res.locals.message = err.message
   res.locals.error = req.app.get('env') === 'development' ? err : {}
-
   // render the error page
   res.status(err.status || 500)
   res.render('error')
@@ -49,7 +48,7 @@ app.use(passport.session())
 
 //SIGN-IN-WITH-GOOGLE
 app.get('/auth/google', AuthController().GoogleOAuth())
-app.get('/auth/google/dotslash', AuthController().GoogleOAuthdotslah)
+app.get('/auth/google/dotslash', AuthController().GoogleOAuthdotslash(res, req))
 //LOGOUT-USER
 app.get('/logout', AuthController().Logout(req, res))
 //REGISTER-NEW-USER
--- api/controllers/AuthController.js
+++ api/controllers/AuthController.js
@@ -1,10 +1,12 @@
-const serverAdmin = process.env.ADMIN
-const accessKey = process.env.AUTH_KEY
-const database = process.env.DB
+require('dotenv').config()
+const mongoose = require('mongoose')
 const passport = require('passport')
 const passportLocalMongoose = require('passport-local-mongoose')
 const GoogleStrategy = require('passport-google-oauth20').Strategy
-const findOrCreate = require('mongoose-findorcreate')
+
+const serverAdmin = process.env.ADMIN
+const accessKey = process.env.AUTH_KEY
+const database = process.env.DB
 
 mongoose.connect(
   'mongodb+srv://' +
@@ -20,66 +22,93 @@ mongoose.connect(
     useUnifiedTopology: true,
   }
 )
+
 mongoose.set('useCreateIndex', true)
 
 //USERS-COLLECTION-SCHEMA
 const userSchema = new mongoose.Schema({
+  firstName: String,
+  lastName: String,
   email: String,
+  verified: Boolean,
   password: String,
   googleId: String,
+  picture: String,
+  language: String,
 })
 
-const AuthController = () => {
-  //SALT-USER-PASSWORD-AND-MORE-WITH-PASSPORT-PLUGIN
-  userSchema.plugin(passportLocalMongoose)
-  userSchema.plugin(findOrCreate)
+//SALT-USER-PASSWORD-AND-MORE-WITH-PASSPORT-PLUGIN
+userSchema.plugin(passportLocalMongoose)
 
-  //USER-DATA-MODEL
-  const User = new mongoose.model('user', userSchema)
+//USER-DATA-MODEL
+const User = new mongoose.model('user', userSchema)
 
-  //LOCAL-LOGIN-STRATEGY-AND-COOKIES
-  passport.use(User.createStrategy())
-  passport.serializeUser((user, done) => {
-    done(null, user.id)
+//LOCAL-LOGIN-STRATEGY-AND-COOKIES
+passport.use(User.createStrategy())
+
+passport.serializeUser(function (user, done) {
+  done(null, user.id)
+})
+passport.deserializeUser(function (id, done) {
+  User.findById(id, function (err, user) {
+    done(err, user)
   })
-  passport.deserializeUser((id, done) => {
-    User.findById(id, function (err, user) {
-      done(err, user)
-    })
-  })
+})
 
-  //GOOGLE-SIGN-IN-STRATEGY
-  passport.use(
-    new GoogleStrategy(
-      {
-        clientID: process.env.CLIENT_ID,
-        clientSecret: process.env.CLIENT_SECRET,
-        // TODO: EDIT_URL_FROM_LOCAL_HOST_TO_ACTUAL_DOMAIN_AS_PER_YOUR_API_CREDENTIALS
-        callbackURL: 'http://localhost:3000/auth/google/dotslash',
-        userProfileURL: 'https://www.googleapis.com/oauth2/v3/userinfo',
-      },
-      (accessToken, refreshToken, profile, cb) => {
-        // console.log(profile);
-        User.findOrCreate(
-          { googleId: profile.id, email: profile.email },
-          (err, user) => {
-            return cb(err, user)
-          }
-        )
-      }
-    )
+//GOOGLE-SIGN-IN-STRATEGY
+passport.use(
+  new GoogleStrategy(
+    {
+      clientID: process.env.CLIENT_ID,
+      clientSecret: process.env.CLIENT_SECRET,
+      // TODO: EDIT_URL_FROM_LOCAL_HOST_TO_ACTUAL_DOMAIN_AS_PER_YOUR_API_CREDENTIALS
+      callbackURL: 'http://localhost:3000/auth/google/dotslash',
+      userProfileURL: 'https://www.googleapis.com/oauth2/v3/userinfo',
+    },
+    function (accessToken, refreshToken, profile, done) {
+      User.findOne({ googleId: profile.id }, function (err, user) {
+        if (err) return done(err)
+        if (user) return done(null, user)
+        else {
+          var newUser = new User({
+            googleId: profileData.sub,
+            firstName: profileData.given_name,
+            lastName: profileData.family_name,
+            email: profileData.email,
+            verified: profileData.email_verified,
+            picture: profileData.picture,
+            language: profileData.locale,
+          })
+
+          newUser.save(function (err) {
+            if (err) throw err
+            return done(err, newUser)
+          })
+        }
+      })
+    }
   )
+)
 
-  //SIGN-IN-WITH-GOOGLE
-  const GoogleOAuth = () =>
-    passport.authenticate('google', { scope: ['profile'] })
-
-  const GoogleOAuthdotslah = () => {
+const AuthController = () => {
+  const GoogleOAuth = () => {
     passport.authenticate('google', {
-      failureRedirect: '/login',
-      successRedirect: '/',
+      scope: [
+        'https://www.googleapis.com/auth/userinfo.profile',
+        'https://www.googleapis.com/auth/userinfo.email',
+      ],
     })
   }
+
+  const GoogleOAuthdotslash = (res, req) => {
+    assport.authenticate('google', { failureRedirect: '/login' }),
+      function (req, res) {
+        // SUCCESSFUL-AUTHENTICATION-REDIRECT-HOME
+        res.redirect('/')
+      }
+  }
+
+  p
 
   //LOGOUT-USER
   const Logout = (req, res) => {
@@ -97,7 +126,7 @@ const AuthController = () => {
           console.log(err)
           res.redirect('/register')
         } else {
-          passport.authenticate('local')(req, res, () => {
+          passport.authenticate('local')(req, res, function () {
             res.redirect('/')
           })
         }
@@ -106,16 +135,16 @@ const AuthController = () => {
   }
 
   //LOGIN-USER
-  const LoginUser = (req, res) => {
+  const Login = (req, res) => {
     const newUser = new User({
       username: req.body.username,
       password: req.body.password,
     })
 
-    req.login(newUser, (err) => {
+    req.login(newUser, function (err) {
       if (err) console.log(err)
       else {
-        passport.authenticate('local')(req, res, () => {
+        passport.authenticate('local')(req, res, function () {
           res.redirect('/')
         })
       }
@@ -124,11 +153,11 @@ const AuthController = () => {
 
   return {
     GoogleOAuth,
-    GoogleOAuthdotslah,
+    GoogleOAuthdotslash,
     Logout,
-    LoginUser,
     RegisterNewUser,
+    Login,
   }
 }
 
-module.exports = AuthController
+module.exports = AuthController()
